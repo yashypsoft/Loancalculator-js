0e8b1b13a898d9845d516cf119335e0ad916af36
Yash Prajapati - YPSoft <yashypsoft@gmail.com>
2019-01-11 16:16:30 +0530

    fin

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function (e) {
+    //hide results
+    document.getElementById('results').style.display = 'none';
+    //show result
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults,2000)
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
     console.log('hello');
     //UI variable
     const UIamount = document.getElementById('amount');
@@ -24,16 +31,26 @@ function calculateResults(e){
         UImonthlyPayment.value = monthly.toFixed(2);
         UItotalPayment.value = (monthly*calculatedPayments).toFixed(2);
         UItotalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+        //show result
+        document.getElementById('results').style.display = 'block';
+        //hide loader
+        document.getElementById('loading').style.display = 'none';
     }else{
+        
         showError('Please check your number');
     }
-    e.preventDefault();
+
+
 }
 
 
 //Show Error
 
 function showError(error){
+    //hide result
+    document.getElementById('results').style.display = 'none';
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
     //create a div
     const errorDiv =document.createElement('div');
 
